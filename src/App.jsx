d76e64f360d1d1e08dc2c3916c3888cf7efb6640
KevinJp21 <kevinjp821@gmail.com>
2024-04-12 17:36:19 -0500

    se agregó el datatable

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
 import './App.css'
 import { Suspense } from 'react'
 import {fetchData} from './fetchData'
+import DataTable from 'react-data-table-component';
 
 const apiData = fetchData("http://localhost/Apiresidencia/getpropietario.php")
+
 function App() {
-  const data = apiData.read();
+const datas = apiData.read();
+
+const columns = [
+  {
+    name: 'ID',
+    selector: row => row.id
+  },
+  {
+    name: 'Nombre',
+    selector: row => row.nombres
+  },
+  {
+    name: 'Apellido',
+    selector: row => row.apellidos
+  },
+  {
+    name: 'Direccion',
+    selector: row => row.direccion
+  },
+  {
+    name: 'Email',
+    selector: row => row.correo
+  },
+  {
+    name: 'Telefono',
+    selector: row => row.telefono
+  }
+]
+
   return (
     <div className="App">
       <h1>Fetch Like a PRO</h1>
       <Suspense fallback={<div>Loading...</div>}>
-      <ul className='card'>
-        {data?.map((user) => (
-          <li key={user.id}>{user.nombres} {user.apellidos}</li>
-        ))}
-      </ul>
+      <DataTable
+        columns={columns}
+        data = {datas}
+        pagination
+      />
       </Suspense>
     </div>
   )
