758e10cdabe2df49bfe05b5d147b90c68ef5e973
KevinJp21 <kevinjp821@gmail.com>
2024-04-12 20:18:21 -0500

    Tabla con bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,41 @@
 import './App.css'
 import { Suspense } from 'react'
 import {fetchData} from './fetchData'
-import DataTable from 'react-data-table-component';
-import styled from 'styled-components';
 
 const apiData = fetchData("http://localhost/Apiresidencia/getpropietario.php")
-const customSort = (rows, selector, direction) => {
-	return orderBy(rows, selector, direction);
-};
 function App() {
 const datas = apiData.read();
 
-const columns = [
-  {
-    name: 'ID',
-    selector: row => row.id
-  },
-  {
-    name: 'Nombre',
-    selector: row => row.nombres
-  },
-  {
-    name: 'Apellido',
-    selector: row => row.apellidos
-  },
-  {
-    name: 'Direccion',
-    selector: row => row.direccion
-  },
-  {
-    name: 'Email',
-    selector: row => row.correo
-  },
-  {
-    name: 'Telefono',
-    selector: row => row.telefono
-  }
-]
-
-
   return (
-    <div className="App">
-      <h1>Propietarios Residencias</h1>
-      <Suspense fallback={<div>Loading...</div>}>
-      <DataTable
-        columns={columns}
-        data = {datas}
-        theme='custom'
-        pagination
-        sortFunction={customSort}
-      />
-      </Suspense>
-    </div>
+  <>
+  <table className="table table-striped" id="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Direccion</th>
+        <th scope="col">Telefono</th>
+        <th scope="col">Correo</th>
+        <th scope="col">Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+     {datas.map((datas,id)=>(
+      <tr key={id}>
+        <th scope="row">{datas.id}</th>
+        <td>{datas.nombres}</td>
+        <td>{datas.direccion}</td>
+        <td>{datas.telefono}</td>
+        <td>{datas.correo}</td>
+        <td>
+          <button type="button" className="btn btn-primary">Editar</button>
+          <button type="button" className="btn btn-danger">Borrar</button>
+        </td>
+      </tr>
+     ))}
+    </tbody>
+</table>
+  </>
   )
 }
 
